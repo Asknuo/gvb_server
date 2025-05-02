06e98674f3d144a7e38bae894330b0c78da8174d
Asknuo <3263296956@qq.com>
2025-05-02 14:45:55 +0800

    配置修改

--- api/settings_api/settings_info.go
+++ api/settings_api/settings_info.go
@@ -1,11 +1,12 @@
 package settings_api
 
 import (
+	"gvb_server/global"
 	"gvb_server/models/res"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (SettingsApi) SettingsInfoView(c *gin.Context) {
-	res.OK(map[string]string{}, "xxx", c)
+	res.OKWithData(global.Config.SiteInfo, c)
 }
---
+++ api/settings_api/settings_update.go
@@ -0,0 +1,12 @@
+package settings_api
+
+import (
+	"gvb_server/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (SettingsApi) SettingsInfoUpdateView(c *gin.Context) {
+	var cr config.SiteInfo
+	c
+}
